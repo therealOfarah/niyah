a5109bf3acaf493e30da3c95cbd0cb0a831f8af2
Omar Farah <omaarfaarah2019@gmail.com>
2023-03-17 09:53:43 -0400

    use template with firebase for auth

---
+++ screens/SignUpScreen.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input, Button } from 'react-native-elements';
+import { StackScreenProps } from '@react-navigation/stack';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const auth = getAuth();
+
+const SignUpScreen= ({ navigation }) => {
+  const [value, setValue] = React.useState({
+    email: '',
+    password: '',
+    error: ''
+  })
+
+  async function signUp() {
+    if (value.email === '' || value.password === '') {
+      setValue({
+        ...value,
+        error: 'Email and password are mandatory.'
+      })
+      return;
+    }
+
+    try {
+      await createUserWithEmailAndPassword(auth, value.email, value.password);
+      navigation.navigate('Sign In');
+    } catch (error) {
+      setValue({
+        ...value,
+        error: error.message,
+      })
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Signup screen!</Text>
+
+      {!!value.error && <View style={styles.error}><Text>{value.error}</Text></View>}
+
+      <View style={styles.controls}>
+        <Input
+            value={value.email}
+            onChangeText={(text) => setValue({ ...value, email: text })}
+            variant="outline"
+            placeholder="Username or Email"
+            _light={{
+              placeholderTextColor: "blueGray.400",
+            }}
+            _dark={{
+              placeholderTextColor: "blueGray.50",
+            }}
+
+          />
+        <Input
+          placeholder='Password'
+          containerStyle={styles.control}
+          value={value.password}
+          onChangeText={(text) => setValue({ ...value, password: text })}
+          secureTextEntry={true}
+        />
+
+        <Button title="Sign Up" buttonStyle={styles.control} onPress={signUp} />
+        <Button title="Sign In" buttonStyle={styles.control} onPress={()=>navigation.navigate('Sign In')} />
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+  controls: {
+    flex: 1,
+    width:'100%'
+  },
+
+  control: {
+    marginTop: 10
+  },
+
+  error: {
+    marginTop: 10,
+    padding: 10,
+    color: '#fff',
+    backgroundColor: '#D54826FF',
+  }
+});
+
+export default SignUpScreen;
