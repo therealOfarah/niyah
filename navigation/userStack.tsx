848430c12bf08b49d132d2db71bd2b66aa9443e1
Omar Farah <omaarfaarah2019@gmail.com>
2023-03-17 10:11:27 -0400

    add bottom navbar

--- navigation/userStack.tsx
+++ navigation/userStack.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+// import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from '../screens/Home';
+import Profile from '../screens/Profile';
+import Media from '../screens/Media';
+import Planner from '../screens/Planner';
 
-const Stack = createStackNavigator();
-
+const Tab = createBottomTabNavigator();
 export default function UserStack() {
   return (
+    <>
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home"  options={{headerShown:false}} component={HomeScreen} />
+        <Tab.Screen name="Media"  options={{headerShown:false}} component={Media} />
+        <Tab.Screen name="Planner"  options={{headerShown:false}} component={Planner} />
+        <Tab.Screen name="Profile" options={{headerShown:false}} component={Profile} />
+      </Tab.Navigator>
     </NavigationContainer>
+    </>
   );
 }
---
+++ screens/Qiblah.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Qiblah() {
+  return (
+    <View>
+      <Text>Qiblah</Text>
+    </View>
+  );
+}
